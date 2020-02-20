f5b5978a35e5d5565c121fd84c8e707b86d10f3b
Vee Satayamas <vsatayamas@gmail.com>
2020-02-21 00:57:00 +0700

    router

--- src/cljs/marassapapupphakani/core.cljs
+++ src/cljs/marassapapupphakani/core.cljs
@@ -13,21 +13,81 @@
   [:div
    [:h1 "Auto-complete"]])
 
+(rf/reg-event-db
+ ::initialize-db
+ (fn [_ _]
+   {:current-route nil}))
 
-(defn render
-  []
-  (r/render [ui]
-            (js/document.querySelector "div#app")))
+(rf/reg-event-fx
+ ::navigate
+ (fn [db [_ & route]]
+   {::navigate! route}))
 
 (rf/reg-event-db
- :initialize
- (fn [_ _]
-   {}))
+ ::navigated
+ (fn [db [_ new-match]]
+   (let [old-match   (:current-route db)
+         controllers (reifc/apply-controllers (:controllers old-match) new-match)]
+     (assoc db :current-route
+            (assoc new-match
+                   :controllers
+                   controllers)))))
+
+(rf/reg-sub
+ ::current-route
+ (fn [db]
+   (:current-route db)))
+
+(defn home-page []
+  [:div
+   [:h1 "MAIN"]])
+
+
+(rf/reg-fx
+ ::navigate!
+ (fn [route]
+   (apply reife/push-state route)))
+
+(def routes
+  ["/"
+   [""
+    {:name      ::home
+     :view      home-page
+     :link-text "Home"
+     :controllers
+     []}]])
+
+(defn on-navigate [new-match]
+  (when new-match
+    (rf/dispatch [::navigated new-match])))
+
+(def router
+  (reif/router
+   routes
+   {:data {:coercion reiss/coercion}}))
+
+(defn init-routes! []
+  (reife/start!
+   router
+   on-navigate
+   {:use-fragment false}))
+
+(defn render [{:keys [router]}]
+  (let [current-route @(rf/subscribe [::current-route])]
+    [:div (when current-route
+            [(-> current-route
+                 :data
+                 :view)])]))
+
+(defn mount-root []
+  (rf/clear-subscription-cache!)
+  (init-routes!)
+  (r/render [render {:router router}]
+            (.getElementById js/document "app")))
 
 (defn ^:dev/after-load reload []
-  (rf/clear-subscription-cache!)
-  (render))
+  (mount-root))
 
-(defn init []
-  (rf/dispatch-sync [:initialize])
-  (render))
+(defn ^:export init []
+  (rf/dispatch-sync [::initialize-db])
+  (mount-root))
